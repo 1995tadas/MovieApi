094bd8123b1631c7a78de66d371e70ca7bf714eb
1995tadas <1995tadas@gmail.com>
2021-01-18 02:46:07 +0200

    More responsibility improvements...

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 @section('content')
     <trending-component
-        :translation = '{{ json_encode(trans('movies')) }}'
+        :translation="{{ json_encode(trans('movies')) }}"
         trending-url="{{ route('movies.trending') }}"
-        film-reel = "{{ asset('images\film-reel.png') }}"
+        film-reel="{{ asset('images\film-reel.png') }}"
         show-route="{{ route('movie.show',['id'=> '/']) }}">
     </trending-component>
 @endsection
